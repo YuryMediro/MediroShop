c791152cff37c2b22bc4f1101d3fa31aee1b0c49
Yury Mediro <sagn375@gmail.com>
2025-06-02 13:04:45 +0300

    install shadCN

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
+import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), tailwindcss()],
 	resolve: {
 		alias: {
-			'@app': '/src/app',
-			'@assets': '/src/assets',
-			'@components': '/src/components',
-			'@features': '/src/features',
-			'@hooks': '/src/hooks',
-			'@pages': '/src/pages',
-			'@processes': '/src/processes',
-			'@shared': '/src/shared',
-			'@utils': '/src/utils',
-			'@widgets': '/src/widgets',
-			'@services': '/src/services/',
-			'@config': '/src/config/',
+			'@': path.resolve(__dirname, './src'),
+			'@components': path.resolve(__dirname, './src/components'),
+			'@lib': path.resolve(__dirname, './src/lib'),
+			'@app': path.resolve(__dirname, './src/app'),
+			'@assets': path.resolve(__dirname, './src/assets'),
+			'@features': path.resolve(__dirname, './src/features'),
+			'@hooks': path.resolve(__dirname, './src/hooks'),
+			'@pages': path.resolve(__dirname, './src/pages'),
+			'@processes': path.resolve(__dirname, './src/processes'),
+			'@shared': path.resolve(__dirname, './src/shared'),
+			'@utils': path.resolve(__dirname, './src/utils'),
+			'@widgets': path.resolve(__dirname, './src/widgets'),
+			'@services': path.resolve(__dirname, './src/services'),
+			'@config': path.resolve(__dirname, './src/config'),
 		},
 	},
 })
