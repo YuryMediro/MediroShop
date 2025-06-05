53749ab4a925bb68fd4c572ef0c73f814a54e820
Yury Mediro <sagn375@gmail.com>
2025-06-05 18:26:09 +0300

    header

--- src/components/Layouts/store-layout/header/Header.tsx
+++ src/components/Layouts/store-layout/header/Header.tsx
@@ -1,11 +1,38 @@
+import { useProfile } from '@/hooks/useProfile'
 import { MobileSidebar } from '../sidebar/MobileSidebar'
 import s from './Header.module.scss'
+import { Link } from '@tanstack/react-router'
+import { Loader } from './Loader'
+import noImage from '@assets/no-user-image.png'
 
 export const Header = () => {
+	const { user, isLoading } = useProfile()
 	return (
 		<div className={s.header}>
-			Header
 			<MobileSidebar />
+			<div className={s.user}>
+				{isLoading ? (
+					<Loader />
+				) : (
+					user && (
+						<>
+							<Link to='/dashboard' className={s.link}>
+								<p>{user.name}</p>
+								<img
+									src={user.picture}
+									alt={user.name}
+									width={42}
+									height={42}
+									onError={e => {
+										const target = e.target as HTMLImageElement
+										target.src = noImage
+									}}
+								/>
+							</Link>
+						</>
+					)
+				)}
+			</div>
 		</div>
 	)
 }
---
+++ src/components/Layouts/store-layout/header/Loader.tsx
@@ -0,0 +1,13 @@
+import { Skeleton } from '@/components/ui/skeleton'
+
+export function Loader() {
+	return (
+		<div className='flex items-center space-x-4'>
+			<div className='space-y-2'>
+				<Skeleton className='h-4 w-[250px] bg-gray-600' />
+				<Skeleton className='h-4 w-[200px]  bg-gray-600' />
+			</div>
+			<Skeleton className='h-12 w-12 rounded-full  bg-gray-600' />
+		</div>
+	)
+}
