95e42ea76b230d0314cbdeee7694b6adf081b963
Yury Mediro <sagn375@gmail.com>
2025-07-13 12:39:29 +0300

    DataTable PaginationRowModel

--- src/components/ui/DataTable/DataTable.tsx
+++ src/components/ui/DataTable/DataTable.tsx
@@ -9,6 +9,7 @@ import {
 	getSortedRowModel,
 	type ColumnFiltersState,
 	getFilteredRowModel,
+	getPaginationRowModel,
 } from '@tanstack/react-table'
 
 import {
@@ -20,6 +21,20 @@ import {
 	TableRow,
 } from '@components/ui/table'
 import { Input } from '../form-elements/Input'
+import { Button } from '../Button'
+import {
+	ChevronLeft,
+	ChevronRight,
+	ChevronsLeft,
+	ChevronsRight,
+} from 'lucide-react'
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from '../select'
 
 interface DataTableProps<TData, TValue> {
 	columns: ColumnDef<TData, TValue>[]
@@ -44,6 +59,7 @@ export function DataTable<TData, TValue>({
 		getSortedRowModel: getSortedRowModel(),
 		onColumnFiltersChange: setColumnFilters,
 		getFilteredRowModel: getFilteredRowModel(),
+		getPaginationRowModel: getPaginationRowModel(),
 		state: {
 			sorting,
 			columnFilters,
@@ -115,6 +131,76 @@ export function DataTable<TData, TValue>({
 						)}
 					</TableBody>
 				</Table>
+			</div>
+			<div className='flex items-center space-x-6 lg:space-x-8 justify-end py-4'>
+				<div className='flex items-center space-x-2'>
+					<p className='text-sm font-medium'>Строк на странице</p>
+					<Select
+						value={`${table.getState().pagination.pageSize}`}
+						onValueChange={value => {
+							table.setPageSize(Number(value))
+						}}
+					>
+						<SelectTrigger className='h-8 w-[70px]'>
+							<SelectValue placeholder={table.getState().pagination.pageSize} />
+						</SelectTrigger>
+						<SelectContent side='top'>
+							{[10, 20, 25, 30, 40, 50].map(pageSize => (
+								<SelectItem key={pageSize} value={`${pageSize}`}>
+									{pageSize}
+								</SelectItem>
+							))}
+						</SelectContent>
+					</Select>
+				</div>
+				<div className='flex w-[130px] items-center justify-center text-sm font-medium'>
+					Страница {table.getState().pagination.pageIndex + 1} из{' '}
+					{table.getPageCount()}
+				</div>
+				<div className='flex items-center space-x-2'>
+					<Button
+						variant='outline'
+						size='icon'
+						className='hidden size-8 lg:flex'
+						onClick={() => table.setPageIndex(0)}
+						disabled={!table.getCanPreviousPage()}
+					>
+						<span className='sr-only'>Перейти на первую страницу</span>
+						<ChevronsLeft />
+					</Button>
+					<Button
+						variant='outline'
+						size='icon'
+						className='size-8'
+						onClick={() => table.previousPage()}
+						disabled={!table.getCanPreviousPage()}
+					>
+						<span className='sr-only'>Перейти на предыдущую страницу</span>
+						<ChevronLeft />
+					</Button>
+					<Button
+						variant='outline'
+						size='icon'
+						className='size-8'
+						onClick={() => table.nextPage()}
+						disabled={!table.getCanNextPage()}
+					>
+						<span className='sr-only'>
+							Перейти на следующую страницуПерейти на следующую страницу
+						</span>
+						<ChevronRight />
+					</Button>
+					<Button
+						variant='outline'
+						size='icon'
+						className='hidden size-8 lg:flex'
+						onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+						disabled={!table.getCanNextPage()}
+					>
+						<span className='sr-only'>Перейти на последнюю страницу</span>
+						<ChevronsRight />
+					</Button>
+				</div>
 			</div>
 		</div>
 	)
