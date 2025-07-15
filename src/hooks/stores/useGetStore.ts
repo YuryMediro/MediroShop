dd4c72965fa40c76f11522104d60b050a49c05e4
Yury Mediro <sagn375@gmail.com>
2025-07-15 17:34:15 +0300

    commit useGetStore

--- src/hooks/stores/useGetStore.ts
+++ src/hooks/stores/useGetStore.ts
@@ -1,14 +1,14 @@
-import { storeService } from '@/services/store.service'
-import { useQuery } from '@tanstack/react-query'
-import { useParams } from '@tanstack/react-router'
+// import { storeService } from '@/services/store.service'
+// import { useQuery } from '@tanstack/react-query'
+// import { useParams } from '@tanstack/react-router'
 
-export default function useGetStore() {
-	const { storeId } = useParams({ from: '/store/$storeId' })
+// export default function useGetStore() {
+// 	const { storeId } = useParams({ from: '/store/$storeId' })
   
-	const { data: stores } = useQuery({
-		queryKey: ['get store'],
-		queryFn: () => storeService.getById(storeId),
-	})
+// 	const { data: stores } = useQuery({
+// 		queryKey: ['get store'],
+// 		queryFn: () => storeService.getById(storeId),
+// 	})
 
-	return { stores }
-}
+// 	return { stores }
+// }
